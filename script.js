1856c5f0bc803560f1b6e826d2a0624f0b9c2c60
Sayed Mohamed <73748545+sayedmuhammed74@users.noreply.github.com>
2022-12-29 06:28:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-let hearts = document.querySelectorAll('.fa-heart')
+let hearts = document.querySelectorAll('.fa-heart');
+let commentsIcon = document.querySelectorAll('.commentsIcon');
 
-
-hearts.forEach(e => {
-    e.onclick = function() {
-        if(e.classList.contains('fa-regular')){
-            e.style.color = "red";
-            e.classList.remove('fa-regular')
-            e.classList.add('fa-solid')
-            console.log('regular')
-        } else {
-            e.style.color = "black";
-            e.classList.add('fa-regular')
-            e.classList.remove('fa-solid')
-            console.log('solid')
-        }
+let post = document.querySelector('.post');
+hearts.forEach((e) => {
+  e.onclick = function () {
+    if (e.classList.contains('fa-regular')) {
+      e.style.color = 'red';
+      e.classList.remove('fa-regular');
+      e.classList.add('fa-solid');
+      console.log('regular');
+    } else {
+      e.style.color = 'black';
+      e.classList.add('fa-regular');
+      e.classList.remove('fa-solid');
+      console.log('solid');
     }
+  };
+});
+
+commentsIcon.forEach((e) => {
+  e.addEventListener('click', () => {
+    e.nextElementSibling.classList.toggle('show-comments');
+    if (e.nextElementSibling.classList.contains('show-comments')) {
+      commentsIcon.textContent = 'hide comments';
+      e.parentElement.style.height = '570px';
+    } else {
+      commentsIcon.textContent = 'view all 277 comments';
+      e.parentElement.style.height = '480px';
+    }
+  });
 });
